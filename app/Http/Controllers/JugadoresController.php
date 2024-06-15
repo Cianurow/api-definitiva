c645f8a92e0e17024ed75d96442b40a225555ccb
Matias Ojeda <matias.ojeda.saa@gmail.com>
2024-06-14 20:34:17 -0400

    api

---
+++ app/Http/Controllers/JugadoresController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Jugador;
+use Illuminate\Http\Request;
+
+class JugadoresController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        // Obtener el parámetro 'equipo_id' de la solicitud
+        $equipoId = $request->query('equipo_id');
+
+        // Si se proporciona 'equipo_id', filtrar los jugadores
+        if ($equipoId) {
+            $jugadores = Jugador::where('equipo_id', $equipoId)->get();
+        } else {
+            // De lo contrario, devolver todos los jugadores
+            $jugadores = Jugador::all();
+        }
+
+        return response()->json($jugadores);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Jugador $jugador)
+    {
+        return $jugador;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Jugador $jugador)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Jugador $jugador)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Jugador $jugador)
+    {
+        //
+    }
+}
